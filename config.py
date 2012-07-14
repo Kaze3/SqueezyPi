f2222b48faad20c59968d867738c7c75a936313b
Matt Hand <matt.kaze3@gmail.com>
2012-07-14 21:37:17 +0100

    ConfigLoader now uses instance variables. Fleshed out SqueezeslaveMonitor.

--- config.py
+++ config.py
@@ -4,14 +4,6 @@ from json import JSONDecoder
 
 class ConfigLoader:
     """Class for handling the SqueezyPi JSON configuration file."""
-    server_hostname = ""
-    server_port = 9090
-    server_username = ""
-    server_password = ""
-    
-    player_name = ""
-
-    squeezypi_use_local_pylms = 0
 
     def load_config(self, config_path='Config.json'):
         """Load the configuration file."""
@@ -20,11 +12,11 @@ class ConfigLoader:
         data = f.read()
         f.close()
 
-        server_hostname = decoder.decode(data)['Server']['hostname']
-        server_port = decoder.decode(data)['Server']['port']
-        server_username = decoder.decode(data)['Server']['username']
-        server_password = decoder.decode(data)['Server']['password']
+        self.server_hostname = decoder.decode(data)['Server']['hostname']
+        self.server_port = decoder.decode(data)['Server']['port']
+        self.server_username = decoder.decode(data)['Server']['username']
+        self.server_password = decoder.decode(data)['Server']['password']
         
-        player_name = decoder.decode(data)['Player']['name']
+        self.player_name = decoder.decode(data)['Player']['name']
         
-        squeezypi_use_local_pylms = decoder.decode(data)['SqueezyPi']['use_local_pylms']
+        self.squeezypi_use_local_pylms = decoder.decode(data)['SqueezyPi']['use_local_pylms']
--- monitor.py
+++ monitor.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
+import os.path
+import sys
+
 class SqueezeslaveMonitor:
     def __init__(self, use_local_pylms=False):
+        if use_local_pylms:
+            basepath = os.path.dirname(__file__)
+            filepath = os.path.abspath(os.path.join(basepath, "..", "PyLMS"))
+            sys.path.append(filepath) 
+        
+        from pylms.server import Server
+        from pylms.player import Player
+    
+    def connect_server(self, server_hostname, server_port, server_username, server_password):
+        """Connect to a Logitech Media server."""
+        self.server = Server(hostname=server_hostname, port=server_port, username=server_username, password=server_password)
+        self.server.connect()
 
-    def connect_server(self, hostname, port, username, password):
+    def connect_player(self, player_name):
+        """Connect to the Squeezeslave player (must be connected to a server)."""
+        self.player = self.server.get_player(player_name)
 
-    def connect_player(self, name):
-
-    def begin_monitoring(self):
+    #def begin_monitoring(self):
