a4565779345568a5afb685288546208e0e68c1c2
Ben Green <bengreen.uk@gmail.com>
2012-07-15 17:39:48 +0100

    Introduced classes for each of the server, player and program configuration variables

--- config.py
+++ config.py
@@ -5,18 +5,33 @@ from json import JSONDecoder
 class ConfigLoader:
     """Class for handling the SqueezyPi JSON configuration file."""
 
-    def load_config(self, config_path='Config.json'):
+    def __init__(self, config_path='Config.json'):
         """Load the configuration file."""
-        decoder = JSONDecoder()
         f = open(config_path, "r")
         data = f.read()
         f.close()
 
-        self.server_hostname = decoder.decode(data)['Server']['hostname']
-        self.server_port = decoder.decode(data)['Server']['port']
-        self.server_username = decoder.decode(data)['Server']['username']
-        self.server_password = decoder.decode(data)['Server']['password']
-        
-        self.player_name = decoder.decode(data)['Player']['name']
-        
-        self.squeezypi_use_local_pylms = decoder.decode(data)['SqueezyPi']['use_local_pylms']
+        self.server = self.ServerConfig(data)
+        self.player = self.PlayerConfig(data)
+        self.squeezy_pi = self.SqueezyPiConfig(data)
+
+    class ServerConfig:
+        """Load and store server configuration variables"""
+        def __init__(self, data):
+            decoder = JSONDecoder()
+            self.hostname = decoder.decode(data)['Server']['hostname']
+            self.port = decoder.decode(data)['Server']['port']
+            self.username = decoder.decode(data)['Server']['username']
+            self.password = decoder.decode(data)['Server']['password']
+
+    class PlayerConfig:
+        """Load and store player configuration variables"""
+        def __init__(self, data):
+            decoder = JSONDecoder()
+            self.name = decoder.decode(data)['Player']['name']
+
+    class SqueezyPiConfig:
+        """Load and store SqueezyPi configuration variables"""
+        def __init__(self, data):
+            decoder = JSONDecoder()
+            self.use_local_pylms = decoder.decode(data)['SqueezyPi']['use_local_pylms']
